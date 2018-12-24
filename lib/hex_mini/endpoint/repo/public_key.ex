5fa37e2656799feee607e9d7907e06005ad266d4
Yuri Artemev <i@artemeff.com>
2018-12-24 20:22:21 +0300

    public key should be always exist

--- lib/hex_mini/endpoint/repo/public_key.ex
+++ lib/hex_mini/endpoint/repo/public_key.ex
@@ -4,14 +4,8 @@ defmodule HexMini.Endpoint.Repo.PublicKey do
   def init(_), do: []
 
   def call(conn, _opts) do
-    case HexMini.public_key do
-      nil ->
-        send_resp(conn, 404, "public key not found")
-
-      key ->
-        conn
-        |> put_resp_header("content-type", "application/x-pem-file")
-        |> send_resp(200, key)
-    end
+    conn
+    |> put_resp_header("content-type", "application/x-pem-file")
+    |> send_resp(200, HexMini.public_key)
   end
 end
