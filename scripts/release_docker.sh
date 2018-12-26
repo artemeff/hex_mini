90ed1a4791721cf85680fa5dc7a89038f14f1844
Yuri Artemev <i@artemeff.com>
2018-12-26 18:21:43 +0300

    provide app_name to release docker script

--- scripts/release_docker.sh
+++ scripts/release_docker.sh
@@ -2,5 +2,5 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build -t $DOCKER_USERNAME/$APP_NAME:$1 .
-docker push $DOCKER_USERNAME/$APP_NAME:$1
+docker build -t $DOCKER_USERNAME/hex_mini:$1 .
+docker push $DOCKER_USERNAME/hex_mini:$1
